ba295922c47b0ba7c9454869abb065c9f278c5c7
sunnkr5 <sunnkr5@gmail.com>
2025-02-20 18:03:17 +0530

    passed all the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let rvrsArray = string.split("").reverse();
+    let joinArray = rvrsArray.join("");
+    return joinArray;
+}
+console.log(reverseString("Sunny"));
 
 // Do not edit below this line
 module.exports = reverseString;
