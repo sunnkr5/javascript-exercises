a2d165d7ee925a4ef95bbec5a4c69763567ca228
sunnkr5 <sunnkr5@gmail.com>
2025-02-20 12:02:23 +0530

    tried to solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+function repeatString(string, num) {
+let result = "";
+    for (let index = 0; index < num; index++) {
+    result += string;
+    }
+return result;
+}
 
-};
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
