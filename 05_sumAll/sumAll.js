3e6b8ed1c38d7b04f5bcd9f44fe9c19bddc62201
sunnkr5 <sunnkr5@gmail.com>
2025-02-21 13:16:53 +0530

    written the code for ex-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
-};
+const sumAll = function (start, end) {
+    const result = [];
+    for (let i = start; i <= end; i++) {
+      result.push(i);
+    }
+      let sum = result.reduce((a, b) => a + b);
+    return sum;
+  }
+  
+  console.log(sumAll(1, 10));
 
 // Do not edit below this line
 module.exports = sumAll;
