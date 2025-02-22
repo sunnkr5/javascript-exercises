834053910634e94352a46f524cd9f76edea5809f
sunnkr5 <sunnkr5@gmail.com>
2025-02-22 12:01:08 +0530

    written the code for ex-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+      let tempInCelsius = (tempInFahrenheit - 32) * 5 / 9;
+    return Math.round(tempInCelsius);
+}
+console.log(convertToCelsius(32));
+
+const convertToFahrenheit = function(tempInCelsius) {
+  let tempInFahrenheit = (tempInCelsius * 9 / 5 + 32);
+  return Math.round(tempInFahrenheit);
 };
 
-const convertToFahrenheit = function() {
-};
+console.log(convertToFahrenheit(0));
+
+
 
 // Do not edit below this line
 module.exports = {
