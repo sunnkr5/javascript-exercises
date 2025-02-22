d17da61ea8e8654e4e9d082081e278c082cdd8b1
sunnkr5 <sunnkr5@gmail.com>
2025-02-22 11:01:13 +0530

    written the code for ex-6 and passed the 1st test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(anyYear) {
+    if (anyYear % 4 === 0 && anyYear % 100 !== 0 || anyYear % 400 === 0) {
+        return true;
+        } return false;
+    
 };
+
+console.log(leapYears(3001));
 
 // Do not edit below this line
 module.exports = leapYears;
