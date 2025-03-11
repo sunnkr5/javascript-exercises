9dee7e9d047febee444e6bff0d96b2a2f4e5f66b
sunnkr5 <sunnkr5@gmail.com>
2025-03-11 15:42:20 +0530

    completed all the function of this exercise now going to test it

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -5,25 +5,42 @@ console.log(add(5, 7));
 
 
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(a, b) {
+	return a - b;
 
 };
 
-const power = function() {
-	
+console.log(subtract(5, 7));
+
+const sum = function(a, b) {
+	return a + b;
+
 };
 
-const factorial = function() {
-	
+console.log(sum(5, 7));
+
+const multiply = function(a, b) {
+return a * b;
 };
+
+console.log(multiply(5, 2));
+
+const power = function(a, b) {
+	return Math.pow(a, b);
+};
+
+console.log(power(4, 3));
+
+const factorial = function(numb) {
+  let result = 1;
+  for (let index = 1; index <= numb; index++) {
+    result *= index;
+     }
+     return result;
+
+};
+
+console.log(factorial(4));
 
 // Do not edit below this line
 module.exports = {
