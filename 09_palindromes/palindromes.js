ebca77d98c7d93f58abba07e8b763bfdd7570c03
sunnkr5 <sunnkr5@gmail.com>
2025-03-12 11:36:31 +0530

    written the code going to check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
 const palindromes = function () {
-
-};
+    let input1 = "madam";
+    let revInput = input1.split('').reverse().join('');
+    console.log(revInput);
+    
+    if (input1 === revInput) {
+        console.log(`${input1} is a palindrome`);
+    } else {
+      console.log(`not a plindrome`);
+    }
+    };
+    palindromes();
+    
 
 // Do not edit below this line
 module.exports = palindromes;
