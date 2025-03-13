7a4bedb12970d81529669e6ac208032f1a2cd9af
sunnkr5 <sunnkr5@gmail.com>
2025-03-13 11:19:56 +0530

    got the code with help of ai

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = Number(n)
+if(num === 1 || num === 2) {
+    return 1;
+} else if (num === 0) {
+    return 0;
+} else if (num < 0) {
+    return "OOPS";
+}
+let a = 1;
+let b = 1;
+for (let index = 3; index <= num; index++) {
+   let temp = a + b;
+   a = b;
+   b = temp;
+    }
+    return b;
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
