4e58e602244c9457831ccb0785b7d135386154c3
sunnkr5 <sunnkr5@gmail.com>
2025-03-15 10:35:24 +0530

    modified the code and passed the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+
+  const getTheTitles = function(books) {
+return books.map(function(item) {
+    return item.title;
+});
+  };
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
